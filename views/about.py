148ff34268d84812c6ed2cd36ac103a74b812f6a
Arvind Kharwal <karvind629@gmail.com>
2026-04-07 12:48:00 +0530

    Streamlit Project 7 April 2026

--- views/about.py
+++ views/about.py
@@ -1,3 +1,6 @@
 import streamlit as st
 st.title("About Page")
-st.header("This is my header")
+st.header("This is about us")
+col1,col2 = st.columns(2)
+col1.write("This is column 1")
+col2.write("This is column 2")
