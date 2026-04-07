2c8169c221741296c47b4d2cdd54e3e0eec4264d
Arvind Kharwal <karvind629@gmail.com>
2026-04-07 12:49:31 +0530

    Streamlit Project 7 April 2026

--- views/about.py
+++ views/about.py
@@ -2,5 +2,5 @@ import streamlit as st
 st.title("About Page")
 st.header("This is about us")
 col1,col2 = st.columns(2)
-col1.write("This is column 1")
-col2.write("This is column 2")
+col1.text("This is column 1")
+col2.subheade("This is column 2")
